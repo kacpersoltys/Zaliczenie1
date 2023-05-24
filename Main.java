3012bf69916fda83985e0277b6bf3ea6d9cbcff4
kacpersoltys <126696092+kacpersoltys@users.noreply.github.com>
2023-05-24 14:31:59 +0000

    .

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+import java.util.Scanner;
 
-,.
+class Main {
+    public static void main(String args[])
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe");
+        double liczba = scanner.nextDouble();
+    
+        if (liczba < 0){
+            System.out.println("Liczba jest mniejsza od zera");
+        } else if (liczba > 0) {
+            System.out.println("Liczba jest wieksza od zera");
+        }
+        else{
+            System.out.println("Liczba jest rowna zero");
+        }
+
+    }
+
+}
